f8ca40dffa964778a4763886d88e988ad9385094
rossgb <r0554613@gmail.com>
2015-07-04 16:34:23 -0700

    player state implemented

--- classes/player.cpp
+++ classes/player.cpp
@@ -1,4 +1,8 @@
 #include "player.h"
+#include "iostream"
+
+static const float SCALE = 30.f;
+
 
 Player::Player(b2World& World) {
   b2BodyDef bodyDef;
@@ -7,11 +11,109 @@ Player::Player(b2World& World) {
   bodyDef.angle = 0; //set the starting angle
 
   this->body = World.CreateBody(&bodyDef);
+
+  b2PolygonShape Shape;
+  Shape.SetAsBox((32.f/2)/SCALE, (32.f/2)/SCALE);
+  b2FixtureDef FixtureDef;
+  FixtureDef.density = 1.f;
+  FixtureDef.friction = 0.7f;
+  FixtureDef.shape = &Shape;
+  this->body->CreateFixture(&FixtureDef);
+  this->state = NONE;
 }
 Player::~Player() {}
 
-void Player::update() {
+void Player::update(sf::RenderWindow &Window) {
   //handle keyboard
-  //handle collisions (with box2d)
-  //hangle animator calls
+  handleKeyboard(Window);
+
+  handleState();
+
+  handlePhysics();
+
+  //get textures from animator and render it to the players position
+  handleAnimation(Window);
+
+  //logging
+  std::cout<< this->body->GetPosition().x << std::endl;
+}
+
+
+void Player::handlePhysics() {
+
+}
+
+void Player::handleAnimation(sf::RenderWindow &Window) {
+    sf::Sprite Sprite;
+    //Sprite.setTexture(animator.getTexture());
+    Sprite.setOrigin(16.f, 16.f);
+    Sprite.setPosition(SCALE * this->body->GetPosition().x, SCALE * this->body->GetPosition().y);
+    Sprite.setRotation(this->body->GetAngle() * 180/b2_pi);
+    Window.draw(Sprite);
+}
+
+void Player::handleState() {
+
+}
+
+void Player::handleKeyboard(sf::RenderWindow &Window) {
+  sf::Event event;
+
+  while (Window.pollEvent(event))
+  {
+      // check the type of the event...
+      switch (event.type)
+      {
+          // key pressed
+          case sf::Event::KeyPressed:
+              if (event.key.code == sf::Keyboard::Left)
+              {
+                  this->state += LEFT;
+              }
+              if (event.key.code == sf::Keyboard::Right)
+              {
+                  this->state += RIGHT;
+              }
+              if (event.key.code == sf::Keyboard::Up)
+              {
+                  this->state += UP;
+              }
+              if (event.key.code == sf::Keyboard::Down)
+              {
+                  this->state += DOWN;
+              }
+              if (event.key.code == sf::Keyboard::Space)
+              {
+                  this->state += SPACE;
+              }
+              break;
+          case sf::Event::KeyReleased:
+              if (event.key.code == sf::Keyboard::Left)
+              {
+                  this->state -= LEFT;
+              }
+              if (event.key.code == sf::Keyboard::Right)
+              {
+                  this->state -= RIGHT;
+              }
+              if (event.key.code == sf::Keyboard::Up)
+              {
+                  this->state -= UP;
+              }
+              if (event.key.code == sf::Keyboard::Down)
+              {
+                  this->state -= DOWN;
+              }
+              if (event.key.code == sf::Keyboard::Space)
+              {
+                  this->state -= SPACE;
+              }
+              break;
+
+
+          // we don't process other types of events
+          default:
+              break;
+      }
+  }
 }
--- classes/player.h
+++ classes/player.h
@@ -5,18 +5,31 @@
 #include <Box2D/Box2D.h>
 #include <SFML/Graphics.hpp>
 
+enum StateMask {
+    NONE  = 0x0,
+    LEFT  = 0x1,
+    RIGHT = 0x2,
+    UP    = 0x4,
+    DOWN  = 0x8,
+    SPACE = 0x10
+};
 
 class Player {
 public:
   Player(b2World& World);
   ~Player();
 
-  void update();
+  void update(sf::RenderWindow &Window);
 
   b2Body* body;
   int i;
   sf::Sprite sprite;
+  int state;
 
 private:
+  void handleKeyboard(sf::RenderWindow &Window);  //sets the state based on keyboard input
+  void handleState();                             //resolves conflicting states
+  void handlePhysics();                           //handles movement based on states
+  void handleAnimation(sf::RenderWindow &Window); //gets the texture from the animator based on the state
   //Animation playerAnimation;
 };
